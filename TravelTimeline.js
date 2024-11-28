c62cb7984b0e4dc2cdfb55f7c8e098cae26b81c3
jwwasylenko <jwwasy@gmail.com>
2024-11-27 19:58:27 -0700

    Couldn't figure out responsiveness
    Still have trouble with the foundation of HTML and CSS. Javascript was tricky too.

--- TravelTimeline.js
+++ TravelTimeline.js
@@ -1,36 +1,48 @@
 berlin.addEventListener('click',function (){
     console.log("City being changed");
     document.getElementById("banner").src="images/berlin.png";
+    document.getElementById("CityName").innerHTML="Berlin";
+    console.log("CityName being changed");
+    document.getElementById("anecdotes").innerHTML="It was gloomy and very cloudy while I was in the contemporary underground capital of Germany. You could sense the art scene and artistic creatives pulsating throughout the tall industrial buildings from the cyberpunk futuristic area of Alexanderplatz, to the aesthetics and collaboration involving the East Side Gallery. This city is not for everyone, however the idea that this is the New York of Europe and every artist chasing their dream(s) is captivated to this breathtaking city is truly apparent. A modern day Berlin has an appropriate awareness of its past; this can be seen walking through the Memorial to the Murdered Jews of Europe, strolling past Checkpoint Charlie, or simply gazing up into the sky where the Television Tower dominates the skyline. All stark reminder of the city's past having the Berlin fall in 1989.";
+    console.log("anecdotes changing");
 })
 
 prague.addEventListener('click',function (){
     console.log("City being changed");
     document.getElementById("banner").src="images/prague.png";
+    document.getElementById("CityName").innerHTML="Prague";
+    console.log("CityName being changed");
+    document.getElementById("anecdotes").innerHTML="As someone who first encounter this fairy tale-esque city in 2022, I knew I had to go back. I wasn't going to become an English teacher, so I thought of returning not under the stress of establishing a career. Well I was enamoured with visiting Prague Castle and the famous Astronomical Clock again. The hostel I stayed at was called Czech Inn. I checked in and had a wonderful stay! I have drawings of the magnificent Charles Bridge where it was lively and bustling as always. The city was so charming! So much so the gothic baroque arcitecture shined through the rainy weather. To walk on the same street where Franz Kafka worked at an insurance company (one of my favourite writers) was awe-inspiring to say the least. If I ever get a remote job, you know where you might find me for a little while.";
+    console.log("anecdotes changing");
 
 })
 
 vienna.addEventListener('click',function (){
     console.log("City being changed");
     document.getElementById("banner").src="images/vienna copy.webp";
+    document.getElementById("CityName").innerHTML="Vienna";
+    console.log("CityName being changed");
+    document.getElementById("anecdotes").innerHTML="Decadence at it's finest ladies and gentleman. As a result of being a bohemian drifer from the last city, I couldn't afford the luxury that doubles as the City of Music and Dreams. I can't believe I'm sharing this because I went on Google to examine the hours of operation for the Sigmund Freud Museum and was perceived with deceit and untruthfulness. I was mislead because a member from my tour group told me how amazing the museum was, as well as that it was indeed open! It said it was closed on Saturdays, and I decided to go to the Kunsthistorisches Museum instead having been a secondary option that fateful Saturday. Having known me a while, one could say I love psychology so this was a massive disappointment for me. The silver lining is I saw an amazing piece of art by Pieter Brugel the Elder named The Hunters in the Snow made in 1565.";
+    console.log("anecdotes changing");
 
 })
 
 munich.addEventListener('click',function (){
     console.log("City being changed");
     document.getElementById("banner").src="images/MusicfromMunich.png";
+    document.getElementById("CityName").innerHTML="Munich";
+    console.log("CityName being changed");
+    document.getElementById("anecdotes").innerHTML="Well, this concludes your journey through my travel in Europe during Autumn 2023. I really hope you enjoyed. I was in Munich for one night and had to leave to go the airport the next morning. At that point I was exhausted and ready to return home to Calgary, Alberta, Canada. There was a cool moment when I got to play guitar infront of some hostel guests and sang some old country song. That is the image being shown of me playing guitar, nonetheless I am not very good at all so don't leave this site thinking I have musical abilities because that is not the case. At least on the guitar. Transport to the airport was a headache and I was so happy to return home. That being said the peak of my travels was not at the end rather in the middle. I remember the first and last cities the most and had a great time backpacking Western Europe with G Adventures.";
+    console.log("anecdotes changing");
 
 })
 
 amsterdam.addEventListener('click',function (){
     console.log("City being changed");
     document.getElementById("banner").src="images/Amsterdam.png";
-
-})
-
-const changeText = document.querySelector("change-text");
-
-changeText.addEventListener("click", function (){
-    console.log("Text being changed");
-    changeText.textContent = "Text has been changed!";
+    document.getElementById("CityName").innerHTML="Amsterdam";
+    console.log("CityName being changed");
+    document.getElementById("anecdotes").innerHTML="Arrived on a saturday morning and wandered amongst the active population in Vondelpark. So many cyclists! Also decided on embarking on a visual treat by visiting the Van Gough Museum. So many spectacular paintings and I love his use of the colour yellow personally. Specifially, the yellow house was amazing how he contrasts the blue with the yellow. After being engulfed with artistic compassion, I rode on a bicycle like the Dutch do. Canal after canal, I felt a real appreciation for this mode of transportation. Whether it be planes, trains, cars, or bicycles I was enthralled by the urban metropolosises, and what they had to offer.";
+    console.log("anecdotes changing");
 
 });
